7edc681a9b516b18968295731e9b1a036bb50708
Mark A. Hershberger <mah@nichework.com>
2020-10-21 12:36:39 -0400

    Introduce ns property

---
+++ src/PropertyAnnotators/NamespacePropertyAnnotator.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace SESP\PropertyAnnotators;
+
+use SESP\AppFactory;
+use SESP\PropertyAnnotator;
+use SESP\DatabaseLogReader;
+use SMW\DIWikiPage;
+use SMWDataItem as DataItem;
+use SMW\DIProperty;
+use SMW\SemanticData;
+use Title;
+use User;
+
+/**
+ * @private
+ * @ingroup SESP
+ *
+ * @license GNU GPL v2+
+ */
+class NamespacePropertyAnnotator implements PropertyAnnotator {
+
+	/**
+	 * Predefined property ID
+	 */
+	const PROP_ID = '___NAMESPACE';
+
+	/**
+	 * @var AppFactory
+	 */
+	private $appFactory;
+
+	/**
+	 * @var Integer|null
+	 */
+	private $namespace;
+
+	/**
+	 * @param AppFactory $appFactory
+	 */
+	public function __construct( AppFactory $appFactory ) {
+		$this->appFactory = $appFactory;
+	}
+
+	/**
+	 * @since 2.0
+	 *
+	 * @param User $namespace
+	 */
+	public function setNamespace( $namespace ) {
+		$this->namespace = $namespace;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function isAnnotatorFor( DIProperty $property ) {
+		return $property->getKey() === self::PROP_ID;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function addAnnotation( DIProperty $property, SemanticData $semanticData ) {
+
+		if ( $this->namespace === null ) {
+			$title = $semanticData->getSubject()->getTitle();
+			$nsInfo = MediaWikiServices::getInstance()->getNamespaceInfo();
+
+			$this->namespace = $nsInfo->getCanonicalName( $title->getNamespace() );
+		}
+
+		$dataItem = $this->getDataItem();
+
+		if ( $dataItem ) {
+			$semanticData->addPropertyObjectValue( $property, $dataItem );
+		} else {
+			$semanticData->removeProperty( $property );
+		}
+	}
+
+	private function getDataItem() {
+		if ( $this->namespace instanceof User ) {
+			$userPage = $this->namespace->getUserPage();
+
+			if ( $userPage instanceof Title ) {
+				return DIWikiPage::newFromTitle( $userPage );
+			}
+		}
+	}
+
+}
